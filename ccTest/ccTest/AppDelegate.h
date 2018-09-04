cf099aad3857f4b8959d6cdcce7893a59385e4a5
caiyanpeng <1104990855@qq.com>
2018-09-04 16:54:04 +0800

    initalize project

---
+++ ccTest/ccTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ccTest
+//
+//  Created by Chengguangfa on 2018/9/4.
+//  Copyright © 2018年 com.medosport.mo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ccTest/ccTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ccTest
+//
+//  Created by Chengguangfa on 2018/9/4.
+//  Copyright © 2018年 com.medosport.mo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
